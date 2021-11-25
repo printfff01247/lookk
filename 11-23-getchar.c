a3a6093af723f545a6912aa4ebccdec1234c9d8a
一袋大虾 <94774715+printfff01247@users.noreply.github.com>
2021-11-25 21:34:53 +0800

    Add files via upload

---
+++ 11-23-getchar.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	int ch = 0;
+
+
+
+	////ctrl + z
+	////EOF -end of file (文件结束标志) ->  -1(值为-1)。
+
+	//while((ch = getchar()) != EOF ) //输入
+
+	//{
+	//	putchar(ch);//输出
+	//
+	//}
+
+	return 0;
+}
