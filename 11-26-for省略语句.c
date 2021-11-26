5e62b0569c5b4193c3e913922886b94174302cd8
一袋大虾 <94774715+printfff01247@users.noreply.github.com>
2021-11-26 21:52:30 +0800

    Add files via upload

---
+++ 11-26-for省略语句.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	int i = 0;
+	int j = 0;
+
+	for(; i<10;i++)// for(i=0;i<10;i++)
+	{
+	
+		for(;j<10;j++)//for(j=0;j<10;j++)
+		{
+			printf("hehe\n");
+		
+		}
+
+	}
+
+	return  0;
+}/*for循环的变种，会带来不同的结果*/
