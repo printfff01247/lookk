c66cd714fafbaff559f506d4d98e1ab196f6b9dd
一袋大虾 <94774715+printfff01247@users.noreply.github.com>
2021-12-01 21:47:42 +0800

    Add files via upload

---
+++ 12-1-在数组中查数字.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+int main()
+{
+	//二分查找
+	int arr[] = {1,2,3,4,5,6,7,8,9,10};
+	int k = 7;
+
+	int sz = sizeof(arr)/sizeof(arr[0]);//计算元素个数
+	int left = 0;//左下标
+	int right = sz -1;//右下标
+
+	while(left <= right)
+	{
+		int mid = (left + right)/2;
+		if(arr[mid] > k)
+		{
+			right = mid - 1;
+					
+		}
+		else if(arr[mid] <k)
+		{
+			left = mid+1;
+			
+		}
+		else
+		{
+			printf("找到下标：%d\n", mid);
+			break;
+		}
+		if(left > right)
+		{
+			printf("找不到下标\n");
+		}
+	
+	
+	}
+	return 0;
+
+	//int arr[] = {1,2,3,4,5,6,7,8,9,10};
+	//int k = 7;
+	////写一个代码，在arr数组中（有序的）找到7；
+	//int i = 0;
+	//int sz = sizeof(arr)/sizeof(arr[0]);
+	//for(i = 0;i<sz;i++)
+	//{
+	//	if(k == arr[i])
+	//	{
+	//		printf("找到下标：%d\n",i);
+	//		break;
+	//	}
+	//	
+	//}
+	//if(i == sz)
+	//	printf("未找到\n");
+
+	//		return 0;
+}
