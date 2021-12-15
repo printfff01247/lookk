92ef0b431946300a2749b9343106ca98abc9602e
一袋大虾 <94774715+printfff01247@users.noreply.github.com>
2021-12-15 21:52:27 +0800

    Add files via upload

---
+++ 12-15-函数打印闰年.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int is_leap_year(int y)
+{
+	if((y%4==0&&y%100!=0) ||(y%400==0))  //闰年的判断方式
+		return 1;
+	else
+		return 0;
+}
+
+int main()
+{
+	int year = 0;
+	for(year = 1000;year<=2000;year++)
+	{
+		//判断year是否为闰年
+		if(1 == is_leap_year(year))
+		{
+			printf("%d ",year);
+		
+		}
+	
+	}
+
+	return 0;
+}
